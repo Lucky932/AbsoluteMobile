a4ea31a5d12631d5105d0c7bbed18a91f61664d3
luckt <luckt@DESKTOP-QN4TG2T>
2024-08-08 13:05:47 +0300

    Добавьте файлы проекта.

---
+++ AbsoluteMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AbsoluteMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AbsoluteMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AbsoluteMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
